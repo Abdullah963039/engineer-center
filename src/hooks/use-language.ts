4a8cfbdbe0b8f8637fff49601282d3ce562f3284
Abdullah Al-Waraah <abdullahalwaraa039@gmail.com>
2025-12-18 05:57:44 +0300

    remove files

--- src/hooks/use-language.ts
+++
@@ -1,8 +0,0 @@
-import { Language } from "@/features/translations/lib";
-import { useLocalStorage } from "./use-local-storage";
-
-export function useLanguage() {
-  const [lang, setLang] = useLocalStorage<Language>("eng-center-lang", "en");
-
-  return [lang, setLang] as const;
-}
